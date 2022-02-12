49579a785cc19554c4dfbccd137b8490da21caaf
islom929 <islomsultonov0@gmail.com>
2022-02-12 14:06:42 +0500

    initialize commit

---
+++ js/info.js
@@ -0,0 +1,42 @@
+const carsObj = [
+  {
+    nameCar: 'Damas',
+    price: 1000000,
+    img: ['../img/damas.png']
+  },
+  {
+    nameCar: 'Labo',
+    price: 1000000,
+    img: ['../img/labo.png']
+  },
+  {
+    nameCar: 'Tracker',
+    price: 1000000,
+    img: ['../img/tracker.png']
+  },
+  {
+    nameCar: 'Malibu',
+    price: 1000000,
+    img: ['../img/malibu.png']
+  },
+  {
+    nameCar: 'Equinox',
+    price: 1000000,
+    img: ['../img/equinox.png']
+  },
+  {
+    nameCar: 'Traverse',
+    price: 1000000,
+    img: ['../img/traverse.png']
+  },
+  {
+    nameCar: 'Trailblazer',
+    price: 1000000,
+    img: ['../img/trailblazer.png']
+  },
+  {
+    nameCar: 'Tahoe',
+    price: 1000000,
+    img: ['../img/tahoe.png']
+  }
+]
